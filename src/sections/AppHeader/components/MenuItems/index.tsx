b62552c747aa714e715022b1005da243cb72cf9d
Katie Lamber02 <katielamber02@gmail.com>
2020-09-28 16:31:55 +0300

    X-CSRF-TOKEN in sessions

--- src/sections/AppHeader/components/MenuItems/index.tsx
+++ src/sections/AppHeader/components/MenuItems/index.tsx
@@ -4,7 +4,10 @@ import { useMutation } from "@apollo/react-hooks";
 import { Avatar, Button, Icon, Menu } from "antd";
 import { LOG_OUT } from "../../../../lib/graphql/mutations";
 import { LogOut as LogOutData } from "../../../../lib/graphql/mutations/LogOut/__generated__/LogOut";
-import { displaySuccessNotification, displayErrorMessage } from "../../../../lib/utils";
+import {
+  displaySuccessNotification,
+  displayErrorMessage,
+} from "../../../../lib/utils";
 import { Viewer } from "../../../../lib/types";
 
 interface Props {
@@ -16,9 +19,10 @@ const { Item, SubMenu } = Menu;
 
 export const MenuItems = ({ viewer, setViewer }: Props) => {
   const [logOut] = useMutation<LogOutData>(LOG_OUT, {
-    onCompleted: data => {
+    onCompleted: (data) => {
       if (data && data.logOut) {
         setViewer(data.logOut);
+        sessionStorage.removeItem("token");
         displaySuccessNotification("You've successfully logged out!");
       }
     },
@@ -26,7 +30,7 @@ export const MenuItems = ({ viewer, setViewer }: Props) => {
       displayErrorMessage(
         "Sorry! We weren't able to log you out. Please try again later!"
       );
-    }
+    },
   });
 
   const handleLogOut = () => {
