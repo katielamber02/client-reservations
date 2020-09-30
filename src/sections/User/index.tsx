677e34af21b0e7004da220cfe931e522eeca5d22
Katie Lamber02 <katielamber02@gmail.com>
2020-09-30 12:36:20 +0300

    basic user profile ui

--- src/sections/User/index.tsx
+++ src/sections/User/index.tsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { USER } from ".././../lib/graphql/queries";
+import {
+  User as UserData,
+  UserVariables,
+} from "../../lib/graphql/queries/User/__generated__/User";
+import { RouteComponentProps } from "react-router-dom";
+import { Col, Layout, Row } from "antd";
+import { UserProfile } from "./components";
 
-export const User = () => {
-  return <h2>User</h2>;
+interface MatchParams {
+  id: string;
+}
+const { Content } = Layout;
+export const User = ({ match }: RouteComponentProps<MatchParams>) => {
+  const { data, loading, error } = useQuery<UserData, UserVariables>(USER, {
+    variables: {
+      id: match.params.id,
+    },
+  });
+  const user = data ? data.user : null;
+  const userProfileElement = user ? <UserProfile /> : null;
+
+  return (
+    <Content className="user">
+      <Row gutter={12} justify="space-between">
+        <Col xs={24}>{userProfileElement}</Col>
+      </Row>
+    </Content>
+  );
 };
