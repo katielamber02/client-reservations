0a4da189c34aa56357852080ad0ca01f7594620d
Katie Lamber02 <katielamber02@gmail.com>
2020-10-01 14:48:53 +0300

    Home page Listings of any kind:to fix size

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { HomePriorities } from "./components/HomePriorities";
-import { Layout } from "antd";
+import { Layout, Typography, Col, Row } from "antd";
 import mapBackground from "./assets/map-background.jpg";
-import { RouteComponentProps } from "react-router-dom";
+import { RouteComponentProps, Link } from "react-router-dom";
+import { displayErrorMessage } from "../../lib/utils";
+import sanFransiscoImage from "./assets/san-francisco.jpg";
+import cancunImage from "./assets/cancun.jpg";
 
 const { Content } = Layout;
+const { Paragraph, Title } = Typography;
 
 export const Home = ({ history }: RouteComponentProps) => {
   const onSearch = (value: string) => {
-    history.push(`/listings/${value}`);
+    const trimmedValue = value.trim();
+
+    if (trimmedValue) {
+      history.push(`/listings/${trimmedValue}`);
+    } else {
+      displayErrorMessage("Please enter a valid search!");
+    }
   };
   return (
     <Content
@@ -16,6 +26,50 @@ export const Home = ({ history }: RouteComponentProps) => {
       style={{ backgroundImage: `url(${mapBackground})` }}
     >
       <HomePriorities onSearch={onSearch} />
+      <div className="home__cta-section">
+        <Title level={2} className="home__cta-section-title">
+          Your guide for all things rental
+        </Title>
+        <Paragraph>
+          Helping you make the best decisions in renting your last minute
+          locations.
+        </Paragraph>
+        <Link
+          to="/listings/united%20states"
+          className="ant-btn ant-btn-primary ant-btn-lg home__cta-section-button"
+        >
+          Popular listings in the United States
+        </Link>
+      </div>
+      <div className="home__listings">
+        <Title level={4} className="home__listings-title">
+          Listings of any kind
+        </Title>
+        <Row gutter={12}>
+          <Col xs={24} sm={12}>
+            <Link to="/listings/san%20fransisco">
+              <div className="home__listings-img-cover">
+                <img
+                  src={sanFransiscoImage}
+                  alt="San Fransisco"
+                  className="home__listings-img"
+                />
+              </div>
+            </Link>
+          </Col>
+          <Col xs={24} sm={12}>
+            <Link to="/listings/cancún">
+              <div className="home__listings-img-cover">
+                <img
+                  src={cancunImage}
+                  alt="Cancún"
+                  className="home__listings-img"
+                />
+              </div>
+            </Link>
+          </Col>
+        </Row>
+      </div>
     </Content>
   );
 };
