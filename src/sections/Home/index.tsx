efe7ffc7312da60e42f399b694023cee193fa2a7
Katie Lamber02 <katielamber02@gmail.com>
2020-10-01 14:13:43 +0300

    Home page listings cards displayed

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { HomePriorities } from "./components/HomePriorities";
+import { Layout } from "antd";
 
+const { Content } = Layout;
 export const Home = () => {
-  return <HomePriorities />;
+  return (
+    <Content className="home">
+      <HomePriorities />
+    </Content>
+  );
 };
