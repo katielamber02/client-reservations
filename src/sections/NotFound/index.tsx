1ccb3b54d8d5b99b591b2b990fc7d89b2e08fc1d
Katie Lamber02 <katielamber02@gmail.com>
2020-10-09 10:45:34 +0300

    not found page UI

--- src/sections/NotFound/index.tsx
+++ src/sections/NotFound/index.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Empty, Layout, Typography } from "antd";
+import { Link } from "react-router-dom";
 
+const { Content } = Layout;
+const { Text } = Typography;
 export const NotFound = () => {
-  return <h2>NotFound</h2>;
+  return (
+    <Content className="not-found">
+      <Empty
+        description={
+          <Fragment>
+            <Text className="not-found__description-title">
+              Something went wrong!
+            </Text>
+            <Text className="not-found__description-subtitle">
+              The page you are looking for can't be found!
+            </Text>
+          </Fragment>
+        }
+      />
+      <Link
+        to="/"
+        className="not found__cta ant-btn ant-btn-primary ant-btn-large"
+      >
+        Got to Home Page
+      </Link>
+    </Content>
+  );
 };
